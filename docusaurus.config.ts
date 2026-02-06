e30016ad913f7bdd1973c77f1721c01a5c3e66ce
Loknath <126454723+loknath2002@users.noreply.github.com>
2026-02-06 20:18:16 +0530

    Added Factory Pattern.

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -42,6 +42,8 @@ const config: Config = {
       'classic',
       {
         docs: {
+          showLastUpdateAuthor: true,
+          showLastUpdateTime: true,
           sidebarPath: './sidebars.ts',
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
